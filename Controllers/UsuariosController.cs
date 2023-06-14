80495607d72bd28001909bfacf195ecb54a17996
Rafael Vasconcelos <vasconceloss.raffa@gmail.com>
2023-06-13 22:34:38 -0300

    Tem que terminar

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -11,7 +11,7 @@ using RpgMvc.Models;
 
 namespace RpgMvc.Controllers
 {
-    
+
     public class UsuariosController : Controller
     {
         public string uriBase = "http://anavieira.somee.com/RpgApi/Usuarios/";
@@ -52,7 +52,7 @@ namespace RpgMvc.Controllers
             }
         }
 
-         [HttpGet]
+        [HttpGet]
         public ActionResult IndexLogin()
         {
             return View("AutenticarUsuario");
@@ -73,9 +73,12 @@ namespace RpgMvc.Controllers
                 string serialized = await response.Content.ReadAsStringAsync();
 
                 if (response.StatusCode == System.Net.HttpStatusCode.OK)
-                {   
-                    UsuarioViewModel uLogado = JsonConvert.DeserializeObject<UsuarioViewModel>(serialized);                    
+                {
+                    UsuarioViewModel uLogado = JsonConvert.DeserializeObject<UsuarioViewModel>(serialized);
                     HttpContext.Session.SetString("SessionTokenUsuario", uLogado.Token);
+
+                    HttpContext.Session.SetString("SessionUsername", u.Username);
+
                     TempData["Mensagem"] = string.Format("Bem-vindo {0}!!!", uLogado.Username);
                     return RedirectToAction("Index", "Personagens");
                 }
@@ -90,12 +93,92 @@ namespace RpgMvc.Controllers
                 return IndexLogin();
             }
         }
+        [HttpGet]
+        public async Task<ActionResult> IndexInformacoesAsync()
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                //Novo: Recuperação informação da sessão
+                string login = HttpContext.Session.GetString("SessionUsername");
+                string uriComplementar =
+                $"GetByLogin/{login}";
+                HttpResponseMessage response = await httpClient.GetAsync(uriBase +
+                uriComplementar);
+                string serialized = await response.Content.ReadAsStringAsync();
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    UsuarioViewModel u = await Task.Run(() =>
+                    JsonConvert.DeserializeObject<UsuarioViewModel>(serialized));
+                    return View(u);
+                }
+                else
+                {
+                    throw new System.Exception(serialized);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
 
+        [HttpPost]
+        public async Task<ActionResult> AlterarEmail(UsuarioViewModel u)
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                string token = HttpContext.Session.GetString("SessionTokenUsuario");
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
+                string uriComplementar = "AtualizarEmail";
+                var content = new StringContent(JsonConvert.SerializeObject(u));
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
 
+                HttpResponseMessage response = await httpClient.PutAsync(uriBase + uriComplementar, content);
+                string serialized = await response.Content.ReadAsStringAsync();
 
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)                
+                    TempData["Mensagem"] = "E-mail alterado com sucesso.";                
+                else                
+                    throw new System.Exception(serialized);                
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
 
-
-
+            }
+            return RedirectToAction("IndexInformacoes");
+        }
+        [HttpGet]
+public async Task<ActionResult> ObterDadosAlteracaoSenha()
+{
+UsuarioViewModel viewModel = new UsuarioViewModel();
+try
+{
+HttpClient httpClient = new HttpClient();
+string login = HttpContext.Session.GetString("SessionUsername");
+string uriComplementar = $"GetByLogin/{login}";
+HttpResponseMessage response = await httpClient.GetAsync(uriBase +
+uriComplementar);
+string serialized = await response.Content.ReadAsStringAsync();
+TempData["TituloModalExterno"] = "Alteração de Senha";
+if (response.StatusCode == System.Net.HttpStatusCode.OK)
+{
+viewModel = await Task.Run(() =>
+JsonConvert.DeserializeObject<UsuarioViewModel>(serialized));
+return PartialView("_AlteracaoSenha", viewModel);
+}
+else
+throw new System.Exception(serialized);
+}
+catch (System.Exception ex)
+{
+TempData["MensagemErro"] = ex.Message;
+return RedirectToAction("IndexInformacoes");
+}
+}
     }
 }
